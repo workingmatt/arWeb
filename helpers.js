1d1006d921a54939b7294c1d072a18538ca45943
Matt Mapleston <matt@chanzachanzo.com>
2019-10-17 03:45:15 +0100

    Grab post data, populate postInfoArray, download images into artist folders. Yay

--- helpers.js
+++ helpers.js
@@ -1,5 +1,6 @@
 const https = require('https');
 var request = require('request');
+var fs = require('fs');
 
 module.exports = {
 
@@ -24,10 +25,12 @@ module.exports = {
 				});
 				resp.on('end',function(){
 					console.log("Got data from "+name+" feed");
+					//console.log(data);
 
-					getArtistInfo(data,postArray)
+					getArtistPosts(JSON.parse(data),name,postArray)
 						.then(function(ret){
-							console.log(postArray);
+							console.log("getArtist: postArray");
+							//console.log(postArray);
 							resolve(res.send(ret)); //resolve this getArtist promise
 						})
 					//console.log(data);
@@ -44,12 +47,87 @@ module.exports = {
 }
 
 //Translate feed into postArray
-function getArtistInfo(feed,postArray){
+function getArtistPosts(feed,name,postArray){
 	return new Promise(function(resolve,reject){
 		console.log("getArtistInfo:");
-		
+		var postPromises = new Array;
+		for (var i=0;i<feed.postCount;i++){
+			if(feed.posts[i]!=undefined && feed.posts[i].has_image==1){
+				postPromises.push(getPostInfo(feed, name, i, postArray));
+			}
+		}
 
-		postArray.push("poo");
-		resolve(feed,postArray);
+		Promise.all(postPromises)
+			.then(function(){
+				console.log("Finished getting artist posts");
+				resolve(postArray);
+				// for (var i=0;i<postArray.length-1;i++){
+				// 	downloadImage(postArray[i].image_url,postArray[i].artist,postArray[i].image);
+				// }
+			})
+			.catch(function(err){
+				console.log("Error in getArtistPosts.");
+				console.log(err);
+			});
 	});
+}
+
+function getPostInfo(feed, name, i, postArray){
+	return new Promise(function(resolve,reject){
+		let objPost = new Object();
+
+		objPost.artist = name;
+		objPost.has_image = feed.posts[i].has_image;
+		objPost.image = i;
+		objPost.imageUrl = feed.posts[i].image;
+		objPost.image_width = feed.posts[i].image_width;
+		objPost.image_height = feed.posts[i].image_height;
+
+		objPost.image_large = feed.posts[i].image_large;
+		objPost.image_large_width = feed.posts[i].image_large_width;
+		objPost.image_large_height = feed.posts[i].image_large_height;
+
+		objPost.name = feed.posts[i].user_screen_name;
+		objPost.text = feed.posts[i].text;
+		objPost.network_name = feed.posts[i].network_name;
+		var date = new Date(feed.posts[i].source_created_at);
+		objPost.date = date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear();//date.toLocaleDateString("en-GB");
+		//console.log(objPost.date);
+		postArray.push(objPost);
+
+		var imagePromise = new Array;
+		imagePromise.push(downloadImage(feed.posts[i].image, objPost, i));
+		Promise.all(imagePromise)
+			.then(function(){
+				resolve();
+			})
+			.catch(function(err){
+				console.log("getPostInfo imagePromise Error");
+				console.log(err);
+			});
+		resolve(postArray);
+	});
+}
+
+function downloadImage(url, objPost, index){
+	return new Promise(function(resolve,reject){
+		// console.log("download "+url+" : "+objPost.artist+" : "+" : "+index);
+		let fileStream = fs.createWriteStream(__dirname+"/public/images/"+objPost.artist+"/"+index);
+		request(url, function(err,response,body){
+			if(err){
+				console.log("Error in downloadImage");
+				console.log(err);
+			}
+			objPost.content_type=response.headers['content-type'];
+		}).pipe(fileStream);
+
+		fileStream.on('close', function(){
+			console.log("here");
+			resolve();
+		});
+		fileStream.on('error', function(err){
+			reject(err);
+		});
+	});
+
 }
